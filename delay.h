7351f771ba4ed64b4659b5779aad77df21f948d5
Aditi Mittal <59228353+Aditimittal18@users.noreply.github.com>
2020-08-27 01:50:16 +0530

    Add files via upload
    Embedded C code for Washing Machine!
    PC.0  PC.1-->Motor
    PC.2-->LED
    PD.0-->Increase Spins
    PD.1-->Decrease Spins
    PD.2-->Start
    PD.3-->STOP

---
+++ delay.h
@@ -0,0 +1,14 @@
+// CodeVisionAVR C Compiler
+// (C) 1998-2000 Pavel Haiduc, HP InfoTech S.R.L.
+
+#ifndef _DELAY_INCLUDED_
+#define _DELAY_INCLUDED_
+
+#pragma used+
+
+void delay_us(unsigned int n);
+void delay_ms(unsigned int n);
+
+#pragma used-
+
+#endif
